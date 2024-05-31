ad25d5a50f6dfdc010ee5f4761169a68cd5b7b29
jeffser <jeffrywalfa@gmail.com>
2024-05-30 19:15:19 -0600

    Fixed: Ollama instance wasn't shutting down

--- src/local_instance.py
+++ src/local_instance.py
@@ -7,14 +7,19 @@ port = 11435
 data_dir = os.getenv("XDG_DATA_HOME")
 
 def start():
+    global instance
     instance = subprocess.Popen(["/app/bin/ollama", "serve"], env={**os.environ, 'OLLAMA_HOST': f"127.0.0.1:{port}", "HOME": data_dir}, stderr=subprocess.PIPE, text=True)
     print("Starting Alpaca's Ollama instance...")
     sleep(1)
     print("Started Alpaca's Ollama instance")
 
 def stop():
-    if instance: instance.kill()
-    print("Stopped Alpaca's Ollama instance")
+    global instance
+    if instance:
+        instance.kill()
+        instance.wait()
+        instance = None
+        print("Stopped Alpaca's Ollama instance")
 
 def reset():
     stop()
