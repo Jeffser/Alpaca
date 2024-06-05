60dd1de39d3cf2cd16ca244d413387a1b3ef29ae
jeffser <jeffrywalfa@gmail.com>
2024-06-04 20:00:45 -0600

    Fixed added check so it doesn't fail if there isn't data in a message when converting

--- src/update_history.py
+++ src/update_history.py
@@ -15,19 +15,20 @@ def update(self):
         if not os.path.exists(directory): os.makedirs(directory)
         new_messages = {}
         for message in content['messages']:
-            message_id = self.generate_uuid()
-            if 'images' in message:
-                if not os.path.exists(os.path.join(directory, message_id)): os.makedirs(os.path.join(directory, message_id))
-                new_images = []
-                for image in message['images']:
-                    file_name = f"{self.generate_uuid()}.png"
-                    decoded = base64.b64decode(image)
-                    buffer = io.BytesIO(decoded)
-                    im = Image.open(buffer)
-                    im.save(os.path.join(directory, message_id, file_name))
-                    new_images.append(file_name)
-                message['images'] = new_images
-            new_messages[message_id] = message
+            if message:
+                message_id = self.generate_uuid()
+                if 'images' in message:
+                    if not os.path.exists(os.path.join(directory, message_id)): os.makedirs(os.path.join(directory, message_id))
+                    new_images = []
+                    for image in message['images']:
+                        file_name = f"{self.generate_uuid()}.png"
+                        decoded = base64.b64decode(image)
+                        buffer = io.BytesIO(decoded)
+                        im = Image.open(buffer)
+                        im.save(os.path.join(directory, message_id, file_name))
+                        new_images.append(file_name)
+                    message['images'] = new_images
+                new_messages[message_id] = message
         new_data['chats'][chat_name] = {}
         new_data['chats'][chat_name]['messages'] = new_messages
 
