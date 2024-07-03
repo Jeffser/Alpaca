9a77ea6f9ccb7838b78596afdc62554fe71971d7
jeffser <jeffrysamuer@gmail.com>
2024-07-03 11:49:30 -0600

    Updated languages, added no_NO.po

--- update_translation_files.sh
+++ update_translation_files.sh
@@ -4,9 +4,11 @@ echo "Preparing template..."
 xgettext --output=po/alpaca.pot --files-from=po/POTFILES
 echo "Updating Spanish..."
 msgmerge -U po/es.po po/alpaca.pot
-#echo "Updating Russian..."
-#msgmerge -U po/ru.po po/alpaca.pot
+echo "Updating Russian..."
+msgmerge -U po/ru.po po/alpaca.pot
 echo "Updating French"
 msgmerge -U po/fr.po po/alpaca.pot
 echo "Updating Brazilian Portuguese"
 msgmerge -U po/pt_BR.po po/alpaca.pot
+echo "Updating Norway Norwegian"
+msgmerge -U po/no_NO.po po/alpaca.pot
