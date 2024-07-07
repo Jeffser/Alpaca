783d0f603201df1633e6c924e0d8f49e1834ecfe
jeffser <jeffrysamuer@gmail.com>
2024-07-07 16:08:18 -0600

    Fixed syntaxis errors on nb language file

--- update_translation_files.sh
+++ update_translation_files.sh
@@ -3,14 +3,14 @@ cd "$(dirname "$0")"
 echo "Preparing template..."
 xgettext --output=po/alpaca.pot --files-from=po/POTFILES
 echo "Updating Spanish..."
-msgmerge -U po/es.po po/alpaca.pot
+msgmerge --no-fuzzy-matching -U po/es.po po/alpaca.pot
 echo "Updating Russian..."
-msgmerge -U po/ru.po po/alpaca.pot
+msgmerge --no-fuzzy-matching -U po/ru.po po/alpaca.pot
 echo "Updating French"
-msgmerge -U po/fr.po po/alpaca.pot
+msgmerge --no-fuzzy-matching -U po/fr.po po/alpaca.pot
 echo "Updating Brazilian Portuguese"
-msgmerge -U po/pt_BR.po po/alpaca.pot
+msgmerge --no-fuzzy-matching -U po/pt_BR.po po/alpaca.pot
 echo "Updating Norwegian"
-msgmerge -U po/no.po po/alpaca.pot
+msgmerge --no-fuzzy-matching -U po/nb.po po/alpaca.pot
 echo "Updating Bengali"
-msgmerge -U po/ben.po po/alapca.pot
+msgmerge --no-fuzzy-matching -U po/bn.po po/alpaca.pot
