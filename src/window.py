237e6b475f7e40fa279cf5d2d665e52637d62a0b
jeffser <jeffrysamuer@gmail.com>
2025-01-20 13:17:41 -0600

    Removed thingy I did for testing

--- src/window.py
+++ src/window.py
@@ -442,7 +442,7 @@ class AlpacaWindow(Adw.ApplicationWindow):
             logger.info("Hiding app...")
         else:
             logger.info("Closing app...")
-            if True or any([el for el in list(self.local_model_flowbox) if isinstance(el.get_child(), model_manager_widget.pulling_model)]):
+            if any([el for el in list(self.local_model_flowbox) if isinstance(el.get_child(), model_manager_widget.pulling_model)]):
                 options = {
                     _('Cancel'): {},
                     _('Hide'): {'callback': switch_to_hide},
