0f2fbc215150bccf4772cbd552e7645b1c56cfbd
mags0ft <113589312+mags0ft@users.noreply.github.com>
2025-02-07 01:02:10 +0100

    Extend security policy to reflect where to disclose vulnerabilities (#527)
    * Extend security policy to reflect where to disclose vulnerabilities
    
    * Add missing comma causing bug and further specify translator

--- src/main.py
+++ src/main.py
@@ -55,10 +55,10 @@ translators = [
     'YusaBecerikli (Turkish) https://github.com/YusaBecerikli',
     'Simon (Ukrainian) https://github.com/OriginalSimon',
     'Marcel Margenberg (German) https://github.com/MehrzweckMandala',
-    'Mags0ft (German) https://github.com/mags0ft'
+    'Magnus Schlinsog (German) https://github.com/mags0ft',
     'Edoardo Brogiolo (Italian) https://github.com/edo0',
     'Shidore (Japanese) https://github.com/sh1d0re',
-    'Henk Leerssen (Dutch) https://github.com/Henkster72'
+    'Henk Leerssen (Dutch) https://github.com/Henkster72',
 ]
 
 parser = argparse.ArgumentParser(description="Alpaca")
