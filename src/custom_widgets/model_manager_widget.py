7f2adee210d186cefd4c1227dc815691590f712d
Jeffser <jeffrysamuer@gmail.com>
2025-04-06 13:02:28 -0600

    Added kd shortcut for using tools and change 'Actions' to 'Tools' in model details

--- src/custom_widgets/model_manager_widget.py
+++ src/custom_widgets/model_manager_widget.py
@@ -471,7 +471,7 @@ class category_pill(Adw.Bin):
         'math': {'name': _('Math'), 'css': ['accent'], 'icon': 'accessories-calculator-symbolic'},
         'vision': {'name': _('Vision'), 'css': ['accent'], 'icon': 'eye-open-negative-filled-symbolic'},
         'embedding': {'name': _('Embedding'), 'css': ['error'], 'icon': 'brain-augemnted-symbolic'},
-        'tools': {'name': _('Actions'), 'css': ['accent'], 'icon': 'wrench-wide-symbolic'},
+        'tools': {'name': _('Tools'), 'css': ['accent'], 'icon': 'wrench-wide-symbolic'},
         'small': {'name': _('Small'), 'css': ['success'], 'icon': 'leaf-symbolic'},
         'medium': {'name': _('Medium'), 'css': ['brown'], 'icon': 'sprout-symbolic'},
         'big': {'name': _('Big'), 'css': ['warning'], 'icon': 'tree-circle-symbolic'},
