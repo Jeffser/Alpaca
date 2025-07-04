847fae456343b579f7c837ac36d51a524499e888
Jeffser <jeffrysamuer@gmail.com>
2025-07-04 14:52:44 -0600

    Some fixes for pulling model

--- src/widgets/models/pulling.py
+++ src/widgets/models/pulling.py
@@ -52,25 +52,17 @@ class PullingModelDialog(Adw.Dialog):
 
         main_container.append(status_container)
 
-
-        stop_button = Gtk.Button(
-            child=Adw.ButtonContent(
-                icon_name='media-playback-stop-symbolic',
-                label=_('Stop Download')
-            ),
-            tooltip_text=_('Stop Download'),
-            css_classes=['destructive-action'],
-            halign=3
-        )
-        stop_button.connect('clicked', lambda button: dialog.simple(
-            parent = self.get_root(),
-            heading = _('Stop Download?'),
-            body = _("Are you sure you want to stop pulling '{}'?").format(prettify_model_name(self.model.get_name())),
-            callback = self.stop_download,
-            button_name = _('Stop'),
-            button_appearance = 'destructive'
-        ))
         if self.model.cancellable:
+            stop_button = Gtk.Button(
+                child=Adw.ButtonContent(
+                    icon_name='media-playback-stop-symbolic',
+                    label=_('Stop Download')
+                ),
+                tooltip_text=_('Stop Download'),
+                css_classes=['destructive-action'],
+                halign=3
+            )
+            stop_button.connect('clicked', lambda button: self.model.prompt_stop_download())
             main_container.append(stop_button)
 
         tbv=Adw.ToolbarView()
@@ -92,13 +84,6 @@ class PullingModelDialog(Adw.Dialog):
             follows_content_size=True,
             default_widget=stop_button
         )
-
-    def stop_download(self):
-        local_model_flowbox = self.model.get_parent().get_parent()
-        self.close()
-        if len(list(local_model_flowbox)) == 1:
-            self.get_root().get_application().main_alpaca_window.local_model_stack.set_visible_child_name('no-models')
-        local_model_flowbox.remove(self.model.get_parent())
 
 class PullingModelButton(Gtk.Button):
     __gtype_name__ = 'AlpacaPullingModelButton'
@@ -146,11 +131,20 @@ class PullingModelButton(Gtk.Button):
         self.progressbar = Gtk.ProgressBar(pulse_step=0.5)
         self.progressbar.pulse()
         container.append(self.progressbar)
-        self.dialog = None
         self.digests = []
         self.success_callback = success_callback
         self.cancellable = cancellable
-        self.connect('clicked', lambda btn: self.get_dialog().present(self.get_root()))
+
+        self.dialog = PullingModelDialog(self)
+        self.connect('clicked', lambda btn: self.dialog.present(self.get_root()))
+
+        self.gesture_click = Gtk.GestureClick(button=3)
+        self.gesture_click.connect("released", lambda gesture, n_press, x, y: self.show_popup(gesture, x, y) if n_press == 1 else None)
+        self.add_controller(self.gesture_click)
+        self.gesture_long_press = Gtk.GestureLongPress()
+        self.gesture_long_press.connect("pressed", self.show_popup)
+        self.add_controller(self.gesture_long_press)
+
 
     def get_search_string(self) -> str:
         return '{} {}'.format(self.get_name(), self.model_title)
@@ -158,15 +152,46 @@ class PullingModelButton(Gtk.Button):
     def get_search_categories(self) -> set:
         return set([c for c in available_models.get(self.get_name().split(':')[0], {}).get('categories', []) if c not in ('small', 'medium', 'big', 'huge')])
 
-    def get_dialog(self):
-        if not self.dialog:
-            self.dialog = PullingModelDialog(self)
-        return self.dialog
+
+    def show_popup(self, gesture, x, y):
+        if '{}:latest'.format(self.get_name()) not in list(get_local_models(self)):
+            rect = Gdk.Rectangle()
+            rect.x, rect.y, = x, y
+            actions = [
+                [
+                    {
+                        'label': _('Stop Download'),
+                        'callback': lambda: self.prompt_stop_download(),
+                        'icon': 'media-playback-stop-symbolic'
+                    }
+                ]
+            ]
+            popup = dialog.Popover(actions)
+            popup.set_parent(self)
+            popup.set_pointing_to(rect)
+            popup.popup()
+
+    def prompt_stop_download(self):
+        dialog.simple(
+            parent = self.get_root(),
+            heading = _('Stop Download?'),
+            body = _("Are you sure you want to stop pulling '{}'?").format(prettify_model_name(self.get_name())),
+            callback = self.stop_download,
+            button_name = _('Stop'),
+            button_appearance = 'destructive'
+        )
+
+    def stop_download(self):
+        local_model_flowbox = self.get_parent().get_parent()
+        self.dialog.close()
+        if len(list(local_model_flowbox)) == 1:
+            self.get_root().get_application().main_alpaca_window.local_model_stack.set_visible_child_name('no-models')
+        local_model_flowbox.remove(self.get_parent())
 
     def update_progressbar(self, data:dict):
         if not self.get_parent():
             logger.info("Pulling of '{}' was canceled".format(self.get_name()))
-            if self.instance.properties.get('model_directory'):
+            if self.instance and self.instance.properties.get('model_directory'):
                 directory = os.path.join(self.instance.properties.get('model_directory'), 'blobs')
                 for digest in self.digests:
                     files_to_delete = glob.glob(os.path.join(directory, digest + '*'))
@@ -191,17 +216,15 @@ class PullingModelButton(Gtk.Button):
         else:
             if 'total' in data and 'completed' in data:
                 GLib.idle_add(self.progressbar.set_fraction, data.get('completed', 0) / data.get('total', 0))
-                if self.dialog:
-                    GLib.idle_add(self.dialog.progressbar.set_fraction, data.get('completed', 0) / data.get('total', 0))
+                GLib.idle_add(self.dialog.progressbar.set_fraction, data.get('completed', 0) / data.get('total', 0))
             else:
                 GLib.idle_add(self.progressbar.pulse)
-                if self.dialog:
-                    GLib.idle_add(self.dialog.progressbar.pulse)
-            if self.dialog:
-                label_text = [line for line in self.dialog.status_label.get_text().split('\n') if line != '']
-                if data.get('status') and (len(label_text) == 0 or label_text[-1] != data.get('status')):
-                    label_text.append(data.get('status'))
-                    GLib.idle_add(self.dialog.status_label.set_label, '\n'.join(label_text))
+                GLib.idle_add(self.dialog.progressbar.pulse)
+
+            label_text = [line for line in self.dialog.status_label.get_text().split('\n') if line != '']
+            if data.get('status') and (len(label_text) == 0 or label_text[-1] != data.get('status')):
+                label_text.append(data.get('status'))
+                GLib.idle_add(self.dialog.status_label.set_label, '\n'.join(label_text))
 
             if data.get('digest') and data.get('digest') not in self.digests:
                 self.digests.append(data.get('digest').replace(':', '-'))
@@ -210,7 +233,8 @@ class PullingModelButton(Gtk.Button):
                 new_model = self.success_callback(self.get_name())
                 if len(get_local_models(self)) > 0:
                     self.get_root().get_application().main_alpaca_window.title_stack.set_visible_child_name('model-selector')
-                self.dialog.close()
+                if self.dialog.get_root():
+                    GLib.idle_add(self.dialog.close)
 
                 dialog.show_notification(
                     root_widget=self.get_root(),
