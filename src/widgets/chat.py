5bfb0d2f91f373704a551a839ba5bdc2a65946bf
Jeffser <jeffrysamuer@gmail.com>
2025-09-01 11:07:03 -0600

    Only load messages if no messages have been loaded before

--- src/widgets/chat.py
+++ src/widgets/chat.py
@@ -281,6 +281,9 @@ class ChatList(Adw.NavigationPage):
                 self.get_root().global_footer.toggle_action_button(False)
             else:
                 self.get_root().global_footer.toggle_action_button(True)
+
+            if len(list(new_chat.container)) == 0:
+                print('load')
                 new_chat.load_messages()
 
             # Show New Stack Page
